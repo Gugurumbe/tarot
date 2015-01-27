bee7b865b579e4d89253e2c2ab7c81b9a87a386d
Vivien Kraus <vivienkro@live.fr>
2015-01-27 18:13:29 +0100

    Version copyleftée :)

--- shared/config.hpp
+++ shared/config.hpp
@@ -1,3 +1,22 @@
+/*
+  Project tarot virtuel is a card game.
+  Copyright (C) 2015  Vivien Kraus
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 /**
    @mainpage PE 46 : tarot virtuel
    @section intro_sec Introduction
@@ -18,8 +37,7 @@
    [.zip](https://github.com/Gugurumbe/tarot/archive/master.zip) 
    ou une archive 
    [.tar.gz](https://github.com/Gugurumbe/tarot/archive/master.tar.gz)
-   est également disponible. Pour le moment, nous n'avons pas réglé
-   le problème de la license. Profitez-en !
+   est également disponible. Le code est sous license GPL.
    Une fois l'archive décompressée, on peut compiler le tout.
    
    @subsection etape2 Étape 2 : Obtenez les outils
--- shared/deboguer.hpp
+++ shared/deboguer.hpp
@@ -1,8 +1,28 @@
+/*
+  Project tarot virtuel is a card game.
+  Copyright (C) 2015  Vivien Kraus
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 /**
    @file shared/deboguer.hpp
    @brief À inclure pour déboguer ce fichier.
 
-   @note Vous devez définir NOM_CLASSE avec la valeur (sous forme de chaîne) du nom de la classe.
+   @note Vous devez définir NOM_CLASSE avec la valeur (sous forme de
+   chaîne) du nom de la classe. 
 */
 
 #include "debogueur.hpp"
